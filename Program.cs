371281481288e85f81ef41129092538b330fa10c
Dark Lc <54839701+BruhMoment4@users.noreply.github.com>
2019-09-13 13:41:50 +0200

    Achievement get Compile This

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Compile_This_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World my name is Nino Scholten");
+        }
+    }
+}
